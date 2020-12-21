74e7747c670ca6109bdcc5089336aef89ad8890a
Leigh Mathieson <leigh.mathieson@gmail.com>
2020-12-21 03:34:14 +0000

    Construct CORS pre-flight OPTIONS headers

--- handlers/getSwaggerDoc/index.js
+++ handlers/getSwaggerDoc/index.js
@@ -1,12 +1,17 @@
 const swaggerDoc = require('./swaggerDoc');
+const CORSHeaders = require('./preFlightRequestHeaders');
 
 exports.handler = (event, context, callback) => {
-    console.log(`Successfully fetched swagger doc: ${JSON.stringify(swaggerDoc, null, 1)}`);
+    const { httpMethod } = event;
+
+    if (httpMethod === 'OPTIONS') callback(null, CORSHeaders)
 
     const response = {
         body: JSON.stringify(swaggerDoc),
         headers: {
-            'Content-Type': 'text/plain'
+            'Content-Type': 'text/plain',
+            "Access-Control-Allow-Origin": "http://the-ui.s3-website.eu-west-2.amazonaws.com",
+            "Access-Control-Allow-Methods": "OPTIONS, GET"
         },
         statusCode: 200,
         isBase64Encoded: false
---
+++ handlers/getSwaggerDoc/preFlightRequestHeaders.js
@@ -0,0 +1,8 @@
+module.exports = {
+    headers: {
+        'Content-Type': 'text/plain',
+        "Access-Control-Allow-Origin": "http://the-ui.s3-website.eu-west-2.amazonaws.com",
+        "Access-Control-Allow-Methods": "OPTIONS, GET"
+    },
+    statusCode: 200
+};
